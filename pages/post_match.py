b1f5ab4c819954a62ef6cff942af5c3727881a3f
EdoardoMarchetti <62024316+EdoardoMarchetti@users.noreply.github.com>
2024-06-27 09:59:15 +0200

    event tagger con pulsantiera

--- pages/post_match.py
+++ pages/post_match.py
@@ -1,9 +1,11 @@
 from st_pages import Page, Section, show_pages, add_page_title
 
 add_page_title()
+
 show_pages(
     [
         Page("app.py", "Event Tagger", "🎦"),
+        Page("pages/event_tagger_handcontrol.py", "Event tagger (Hand control)", "🎦"),
         Page("pages/post_match.py", "Post Match", "⚽"),
     ]
 )
