a2a96d6014f8e6666bd9cfb062962656d2de6f63
EdoardoMarchetti <62024316+EdoardoMarchetti@users.noreply.github.com>
2026-02-20 11:16:31 +0100

    add url to frontend cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,6 +18,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "https://eventtaggerfrontend.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
